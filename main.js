7ab3665f39782b14a0223639a3d4f7df5dd15cc8
Tehreem Javid <tehreemjavid@Tehreems-MacBook-Pro-2.local>
2018-10-29 15:46:40 -0700

    added a way to save the pins and have it show in the console.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var pin = null;
+var clickedImg = '';
 
 document.addEventListener('DOMContentLoaded',() => {
+    getUserBoards(1)
     let cardContainer = document.querySelector('#card-container')
     axios.get('http://localhost:3000/pins').then(result=>{
         let data = result.data
         data.forEach(element=>{
             console.log(element)
+            clickedImg = element.url;
             let div = document.createElement('div')
             div.innerHTML = `
             <div class="card" style="width: 18rem;">
@@ -25,9 +28,11 @@ const getUserBoards = (pinId) => {
     pin = pinId;
     axios.get('http://localhost:3000/user/2/boards').then(result=>{
         const boardsList = document.querySelector('#boardlist');
+        const viewBoards = document.querySelector('#view-boards');
         let boards = result.data;
         boards.forEach(board => {
             boardsList.innerHTML += renderBoardListItem(board);
+            viewBoards.innerHTML += renderViewBoard(board);
         })
         console.log(boards)
 
@@ -38,13 +43,19 @@ const renderBoardListItem = (board) => (`
     <option value="${board.id}">${board.name} - ${board.description}</option>
 `)
 
+const renderViewBoard = (board) => (`
+    <li id="${board.id}">${board.name} - ${board.description}</li>
+`)
+
 const savePinToBoard = () => {
     let boardId = document.getElementById('boardlist').value;
+    console.log('****', boardId);
     axios.post('http://localhost:3000/pins/addpin', {
         pinId: pin,
         boardId
     }).then( result => {
         console.log("I added a pin", result);
+        console.log(clickedImg);
     })
     .catch(error => {
         console.log("I have failed", error);
@@ -52,3 +63,4 @@ const savePinToBoard = () => {
     console.log(pin);
     console.log(boardId);
 }
+
